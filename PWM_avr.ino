7d3b418d4eb8fd45a1df30e6b82806349e2e7ab8
DentAlpha <82549965+DentAlpha@users.noreply.github.com>
2023-06-28 15:36:42 +0700

    Revisi 1

--- PWM_avr.ino
+++ PWM_avr.ino
@@ -25,19 +25,19 @@ void init_timer(){
   // Set prescaler ke 256, CS12 = 1, CS11 = CS10 = 0
   TCCR1B = (1 << CS12);
 
+  // Enable output compare A dengan macro OCIE1A, vektornya pake TIMER1 COMPA
+  TIMSK1 = (1 << OCIE1A);
+
   // Inisialisasi counter, counter = (TCNT1H << 8) + TCNT1L
   TCNT1H = 0x0;
   TCNT1L = 0x0;
 
-  // Set nilai TOP awal yaitu 6249 (0.1 detik), nilai TOP atau isi register OCR1A = (OCR1AH << 8) + OCR1AL
-  OCR1AH = 0b00011000;
-  OCR1AL = 0b01101001;
+  // Set nilai TOP awal yaitu 124 (2 ms), nilai TOP atau isi register OCR1A = (OCR1AH << 8) + OCR1AL
+  OCR1AH = 0;
+  OCR1AL = 0b01111100;
 
   // Enable global interrupt
   SREG = (1 << 7);
-
-  // Enable output compare A dengan macro OCIE1A, vektornya pake TIMER1 COMPA
-  TIMSK1 = (1 << OCIE1A);
 }
 
 
@@ -45,44 +45,38 @@ ISR(TIMER1_COMPA_vect){
   if(flag){
     // Mematikan LED
     flag = 0;
-    PORTB = ~(1 << PB5);
+    PORTB &= ~(1 << PB5);
 
-    // Set OCR1A ke 6249 (0.1 detik)
-    OCR1AH = 0b00011000;
-    OCR1AL = 0b01101001;
+    // Set OCR1A ke 124 (2 ms)
+    OCR1AH = 0;
+    OCR1AL = 0b01111100;
 
   }
   else{
     // Menyalakan LED
     flag = 1;
-    PORTB = 1 << PB5;
+    PORTB |= 1 << PB5;
 
-    // Set OCR1A ke 62499 (1 detik)
-    OCR1AH = 0b11110100;
-    OCR1AL = 0b00100011;
+    // Set OCR1A ke 124 (2 ms)
+    OCR1AH = 0;
+    OCR1AL = 0b01111100;
   }
 }
 
 
 void setup() {
+  // Set pin D13 menjadi output
   DDRD = 0b00000001;
-  // Set pin D13 (atau PB5) menjadi output, sisanya dont care
   DDRB = 0b00100000;
 
-  // Set kondisi awal pin D13 menjadi LOW
+  // Set kondisi awal pin D13 (atau PB5) menjadi LOW
   PORTB = ~(1 << PB5);
 
   // Setting PWM
   init_timer();
-
-  Serial.begin(9600);
 }
 
 
 void loop() {
   // Komentar
-  if(millis() - prevtime > 20){
-    Serial.println(flag);
-    prevtime = millis();
-  }
 }
